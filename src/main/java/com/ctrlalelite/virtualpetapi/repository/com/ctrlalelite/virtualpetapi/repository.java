49f79b74d4b271c1e81f40dddb5b9b22623dbe98
ProgramWithGraham <gaps512@gmail.com>
2024-03-03 15:44:03 -0600

    update

--- src/main/java/com/ctrlalelite/virtualpetapi/repository/com/ctrlalelite/virtualpetapi/repository.java
+++ src/main/java/com/ctrlalelite/virtualpetapi/repository/com/ctrlalelite/virtualpetapi/repository.java
@@ -1,9 +1,9 @@
-package com.ctrlalelite.virtualpetapi.repository.com.ctrlalelite.virtualpetapi;
+package com.ctrlaltelite.virtualpetapi.repository.com.ctrlalelite.virtualpetapi;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//import com.ctrlalelite.virtualpetapi.entity.Repository;
+//import com.ctrlaltelite.virtualpetapi.entity.Repository;
 
 @Repository
 //public interface virtualpetapiRepository extends JpaRepository<Review, Long> {
