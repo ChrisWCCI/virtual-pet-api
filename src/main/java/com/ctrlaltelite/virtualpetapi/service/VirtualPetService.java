0557e37e0c9f8d90e70ba90532782aedc043f0dd
tcollert <tcollert1@gmail.com>
2024-03-03 16:13:33 -0500

    commit

--- src/main/java/com/ctrlaltelite/virtualpetapi/service/VirtualPetService.java
+++ src/main/java/com/ctrlaltelite/virtualpetapi/service/VirtualPetService.java
@@ -1,4 +1,5 @@
 package com.ctrlaltelite.virtualpetapi.service;
+
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -50,6 +51,4 @@ public class VirtualPetService {
         return existingVirtualPet;
     }
 
-    }
-
-
+}
