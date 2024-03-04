99e9b071db154da45a959849aa94d5ea23533530
ProgramWithGraham <gaps512@gmail.com>
2024-03-03 20:32:24 -0600

    updates

--- src/main/java/com/ctrlaltelite/virtualpetapi/controller/VirtualPetController.java
+++ src/main/java/com/ctrlaltelite/virtualpetapi/controller/VirtualPetController.java
@@ -10,15 +10,19 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 
-import com.ctrlaltelite.virtualpetapi.entity.;
-import com.ctrlaltelite.virtualpetapi.service.;
+import com.ctrlaltelite.virtualpetapi.entity.VirtualPet;
+import com.ctrlaltelite.virtualpetapi.service.VirtualPetService;
 
 @RestController
-public class virtualPetController {
+@RequestMapping("/pet")
+
+public class VirtualPetController {
 
     @Autowired
-    UserService userServ;
+    private VirtualPet virtualPet;
 
     @GetMapping("/ping")
     public String pingPong() {
@@ -26,29 +30,28 @@ public class virtualPetController {
     }
 
     @PostMapping("/new")
-    public void addUser(@RequestBody User user) {
-        this.userServ.createUser(user);
+    public void addUser(@RequestBody VirtualPet virtualPet) {
+        this.virtualPet.createUser(virtualPet);
     }
 
     @GetMapping("/users")
-    public List<User> findAllUsers() {
-        return this.userServ.getAllUsers();
+    public List<VirtualPet> findAllUsers() {
+        return this.virtualPet.getAllUsers();
     }
 
     @GetMapping("/user/{id}")
-    public User findUserById(@PathVariable long id) {
-        return this.userServ.getUserById(id);
+    public VirtualPet findUserById(@PathVariable long id) {
+        return this.virtualPet.getUserById(id);
     }
 
     @PutMapping("/user/{id}")
-    public User modifyUser(@PathVariable long id, @RequestBody User updatedUser) {
-        return this.userServ.updateUser(id, updatedUser);
+    public VirtualPet modifyUser(@PathVariable long id, @RequestBody VirtualPet updatedUser) {
+        return this.virtualPet.updateUser(id, updatedUser);
     }
 
-    
     @DeleteMapping("/delete/{id}")
     public void removeUser(@PathVariable long id) {
-        this.userServ.deleteUser(id);
+        this.virtualPet.deleteUser(id);
     }
 
 }
