ec4d8935740bc3706341131bce811a7a0bc5a80f
donaldfletcher11 <donaldfletcher11@outlook.com>
2024-03-05 19:22:56 -0800

    Updated Notes

--- src/main/java/com/ctrlaltelite/virtualpetapi/service/VirtualPetService.java
+++ src/main/java/com/ctrlaltelite/virtualpetapi/service/VirtualPetService.java
@@ -5,8 +5,6 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.server.ResponseStatusException;
 
 import com.ctrlaltelite.virtualpetapi.entity.VirtualPet;
@@ -19,21 +17,21 @@ public class VirtualPetService {
     private VirtualPetRepository virtualPetRepo;
 
     /*
-     * Adds a new User to the repository (db). The "C" (create) in CRUD
+     * Adds a new Pet to the repository (db). The "C" (create) in CRUD
      */
     public void createVirtualPet(VirtualPet virtualPetNew) {
         this.virtualPetRepo.save(virtualPetNew);
     }
 
     /*
-     * Gets all the users in the repo (db). The "R" (read) in CRUD
+     * Gets all the pets in the repo (db). The "R" (read) in CRUD
      */
     public List<VirtualPet> getAllVirtualPets() {
         return this.virtualPetRepo.findAll();
     }
 
     /*
-     * Gets a specific User by its id. The "R" (read) in CRUD
+     * Gets a specific Pet by its id. The "R" (read) in CRUD
      */
     public VirtualPet getVirtualPetById(long id) {
         return this.virtualPetRepo.findById(id)
@@ -41,13 +39,13 @@ public class VirtualPetService {
     }
 
     /*
-     * Updates an existing user, found by a specific id. The "U" (update) in CRUD
+     * Updates an existing Pet, found by a specific id. The "U" (update) in CRUD
      */
     public VirtualPet updateVirtualPet(long id, VirtualPet updatedVirtualPet) {
         VirtualPet existingVirtualPet = this.virtualPetRepo.findById(id)
                 .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Pet not found: " + id));
 
-        // The other way of updating
+        //  Updating Pet Profile
         existingVirtualPet.setId(updatedVirtualPet.getId());
         existingVirtualPet.setPetName(updatedVirtualPet.getPetName());
         existingVirtualPet.setPetDescription(updatedVirtualPet.getPetDescription());
