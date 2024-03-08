654eb1bcb0724da84c35c387fad6df000f81bee8
ProgramWithGraham <gaps512@gmail.com>
2024-03-07 20:38:55 -0600

    b

--- src/main/java/com/ctrlalelite/virtualpetapi/repository/com/ctrlalelite/virtualpetapi/repository.java
+++
@@ -1,9 +0,0 @@
-package com.ctrlalelite.virtualpetapi.repository.com.ctrlalelite.virtualpetapi;
-
-// import com.ctrlalelite.virtualpetapi.entity.Repository;
-
-// @Repository
-
-
-// public interface virtualpetapiRepository extends JpaRepository<Review, Long>
-// {
